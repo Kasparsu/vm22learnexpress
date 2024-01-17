22f8ab0d2d5a0020346014f3e7805b76f12fd66d
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-01-17 14:58:55 +0200

    Add showing simple html

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
   console.log('somebody visited');
 });
 
