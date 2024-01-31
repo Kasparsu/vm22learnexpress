3673b9f6924a3e5f989e71cff0bfa433774399d3
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-01-31 15:13:00 +0200

    Add circle pages

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const nunjucks = require('nunjucks');
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({
+  extended:true
+}));
+
 nunjucks.configure('views', {
     autoescape: true,
     express: app
@@ -21,6 +25,16 @@ app.get('/form', (req, res) => {
   console.log(req.query);
   res.render('form.njk', req.query);
 });
+
+app.get('/circle', (req, res) => {
+  res.render('circle.njk');
+});
+
+app.post('/circle', (req, res) => {
+    let area = Math.PI * req.body.radius * req.body.radius;
+    res.render('circleAnswer.njk', {r: req.body.radius, a: area});
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
