4ac68b57d0630a144f631a36a8628841a8383437
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-02-14 14:37:37 +0200

    Add CRUD

--- src/movieController.js
+++ src/movieController.js
@@ -33,4 +33,44 @@ router.post('/add', (req, res) => {
     res.redirect('/movies/');
 });
 
+router.get('/view', (req, res) => {
+    let id = parseInt(req.query.id);
+    let movies = fs.readFileSync('movies.json', 'utf-8');
+    movies = JSON.parse(movies);
+    let movie = movies.movies.find( m => m.id === id);
+    res.render('movies/view.njk', {movie: movie});
+});
+
+router.get('/edit/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    let movies = fs.readFileSync('movies.json', 'utf-8');
+    movies = JSON.parse(movies);
+    let movie = movies.movies.find( m => m.id === id);
+    res.render('movies/edit.njk', {movie: movie});
+});
+
+router.post('/edit/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    let movies = fs.readFileSync('movies.json', 'utf-8');
+    movies = JSON.parse(movies);
+    let movie = movies.movies.find( m => m.id === id);
+    movie.name = req.body.movie;
+    movie.year = req.body.year;
+    movie.description = req.body.description;
+    let json = JSON.stringify(movies);
+    fs.writeFileSync('movies.json', json);
+    res.redirect('/movies/');
+});
+
+router.get('/delete/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    let movies = fs.readFileSync('movies.json', 'utf-8');
+    movies = JSON.parse(movies);
+    let index = movies.movies.findIndex( m => m.id === id);
+    movies.movies.splice(index, 1);
+    let json = JSON.stringify(movies);
+    fs.writeFileSync('movies.json', json);
+    res.redirect('/movies/');
+});
+
 module.exports = router;
