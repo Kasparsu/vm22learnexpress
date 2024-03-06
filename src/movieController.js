3af2192ae8d354003b656dc9eff69f646397bdde
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-03-06 15:44:00 +0200

    Add database

--- src/movieController.js
+++ src/movieController.js
@@ -1,75 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-router.get('/', (req, res) => {
-    if(!fs.existsSync('movies.json')){
-        let json = {
-            lastId: 0,
-            movies: []
-        };
-        json = JSON.stringify(json);
-        fs.writeFileSync('movies.json', json);
-    }
-    let movies = fs.readFileSync('movies.json', 'utf-8');
-    movies = JSON.parse(movies);
-    res.render('movies/index.njk',{movies: movies.movies});
+const {Sequelize, QueryTypes } = require('sequelize');
+let sequelize = new Sequelize('sqlite:db.sqlite');
+
+router.get('/', async (req, res) => {
+    let movies = await sequelize.query('SELECT * FROM movies;', {type: QueryTypes.SELECT});
+    res.render('movies/index.njk',{movies: movies});
 });
 
 router.get('/add', (req, res) => {
     res.render('movies/add.njk');
 });
 
-router.post('/add', (req, res) => {
-    let movies = fs.readFileSync('movies.json', 'utf-8');
-    movies = JSON.parse(movies);
-    movies.movies.push({
-        id: movies.lastId++,
-        name: req.body.movie,
-        year: req.body.year,
-        description: req.body.description
-    });
-    let json = JSON.stringify(movies);
-    fs.writeFileSync('movies.json', json);
+router.post('/add', async (req, res) => {
+    await sequelize.query(`INSERT INTO movies (name, year, description)
+                           VALUES ('${req.body.movie}', ${req.body.year}, '${req.body.description}');`,
+    {type: QueryTypes.INSERT});
     res.redirect('/movies/');
 });
 
-router.get('/view', (req, res) => {
-    let id = parseInt(req.query.id);
-    let movies = fs.readFileSync('movies.json', 'utf-8');
-    movies = JSON.parse(movies);
-    let movie = movies.movies.find( m => m.id === id);
+router.get('/view', async (req, res) => {
+    let id = req.query.id;
+    let movies = await sequelize.query(`SELECT * FROM movies WHERE id=${id};`, {type: QueryTypes.SELECT});
+    let movie = movies[0];
     res.render('movies/view.njk', {movie: movie});
 });
 
-router.get('/edit/:id', (req, res) => {
+router.get('/edit/:id',async (req, res) => {
     let id = parseInt(req.params.id);
-    let movies = fs.readFileSync('movies.json', 'utf-8');
-    movies = JSON.parse(movies);
-    let movie = movies.movies.find( m => m.id === id);
+    let movies = await sequelize.query(`SELECT * FROM movies WHERE id=${id};`, {type: QueryTypes.SELECT});
+    let movie = movies[0];
     res.render('movies/edit.njk', {movie: movie});
 });
 
-router.post('/edit/:id', (req, res) => {
+router.post('/edit/:id', async (req, res) => {
     let id = parseInt(req.params.id);
-    let movies = fs.readFileSync('movies.json', 'utf-8');
-    movies = JSON.parse(movies);
-    let movie = movies.movies.find( m => m.id === id);
-    movie.name = req.body.movie;
-    movie.year = req.body.year;
-    movie.description = req.body.description;
-    let json = JSON.stringify(movies);
-    fs.writeFileSync('movies.json', json);
+    await sequelize.query(`UPDATE movies
+                            SET name='${req.body.movie}',
+                                year=${req.body.year},
+                                description='${req.body.description}'
+                            WHERE id=${id};`,
+        {type: QueryTypes.UPDATE});
     res.redirect('/movies/');
 });
 
-router.get('/delete/:id', (req, res) => {
+router.get('/delete/:id',async (req, res) => {
     let id = parseInt(req.params.id);
-    let movies = fs.readFileSync('movies.json', 'utf-8');
-    movies = JSON.parse(movies);
-    let index = movies.movies.findIndex( m => m.id === id);
-    movies.movies.splice(index, 1);
-    let json = JSON.stringify(movies);
-    fs.writeFileSync('movies.json', json);
+    await sequelize.query(`DELETE FROM movies WHERE id=${id};`, {type: QueryTypes.DELETE});
     res.redirect('/movies/');
 });
 
